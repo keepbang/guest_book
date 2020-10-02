3b26c176ae269cfc6eb83d0cf281111bdc4215f1
keepbang <rlqud1125@naver.com>
2020-10-02 21:23:09 +0900

    edit updateOne and findId

--- src/index.js
+++ src/index.js
@@ -36,7 +36,28 @@ app.post('/guest_book/insertBook', (req, res) => {
 
 app.post('/guest_book/updateBook', (req, res) => {
 
-    const guestBook = new GuestBook(req.body);
+    if(!req.body._id){
+        return res.status(400).json({
+            success: false,
+            Error_message: "Bad Request : Not Request ('_id')"
+        })
+    }
+
+    let search_id = {
+        _id : req.body._id
+    }
+
+    let update_data = {
+        $set : req.body
+    }
+
+    GuestBook.updateOne(search_id,update_data,(err,result) => {
+        if(err) return res.json({success:false, err});
+        return res.status(200).json({
+            success: true,
+            result
+        })
+    })
     
 })
 
@@ -53,8 +74,19 @@ app.get('/guest_book/selectList', (req, res) => {
 
 })
 
-app.get('/guest_book/select', (req, res) => {
+app.get('/guest_book/selectType', (req, res) => {
     GuestBook.find({type:req.body.type},(err, data) => {
+        if(err) return res.json({success: false, err});
+        return res.status(200).json({
+            success:true,
+            data
+        })
+    });
+
+})
+
+app.get('/guest_book/selectId', (req, res) => {
+    GuestBook.find({_id:req.body._id},(err, data) => {
         if(err) return res.json({success: false, err});
         return res.status(200).json({
             success:true,
